af45729db4936b45f465a7c4ebc14b46b18f46d9
Joe Chapman <joe.chapman3737@gmail.com>
2022-05-20 21:45:00 +1200

    renamed marbles to stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import { getCalculationFn } from './operator-calculations';
-import {
-  createEmptyMarble,
-  createTerminalMarble,
-  MarbleValue,
-  Operations,
-} from './types';
+import { createEmptyMarble, MarbleValue, Operations } from './types';
 
 @Component({
   selector: 'rx-root',
@@ -13,14 +8,14 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public inputMarbles: MarbleValue[] = [
+  public inputStream: MarbleValue[] = [
     createEmptyMarble(),
     createEmptyMarble(),
     createEmptyMarble(),
     createEmptyMarble(),
     createEmptyMarble(),
   ];
-  public outputMarbles: MarbleValue[] = [
+  public outputStream: MarbleValue[] = [
     createEmptyMarble(),
     createEmptyMarble(),
     createEmptyMarble(),
@@ -32,8 +27,8 @@ export class AppComponent {
   public operations: Operations[] = UNARY_OPERATORS;
   public numberOfTick = 5;
 
-  public inputMarblesChanged(marbles: MarbleValue[]): void {
-    this.inputMarbles = marbles;
+  public inputStreamChanged(marbles: MarbleValue[]): void {
+    this.inputStream = marbles;
     this.recalculateOutputMarbles();
   }
 
@@ -43,8 +38,8 @@ export class AppComponent {
   }
 
   private recalculateOutputMarbles(): void {
-    this.outputMarbles = getCalculationFn(this.selectedOperation)(
-      this.inputMarbles
+    this.outputStream = getCalculationFn(this.selectedOperation)(
+      this.inputStream
     );
   }
 }
