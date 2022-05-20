28ab61b8352a45be1ef94cc09357a2d5110fcb91
Joe Chapman <joe.chapman3737@gmail.com>
2022-05-20 21:29:57 +1200

    add min calculation

--- src/app/operator-calculations.ts
+++ src/app/operator-calculations.ts
@@ -39,5 +39,22 @@ export function maxCalculation(input: MarbleValue[]): MarbleValue[] {
 }
 
 export function minCalculation(input: MarbleValue[]): MarbleValue[] {
-  return input.map((_) => createEmptyMarble());
+  const terminalMarble = input.find((marble) => marble.terminal);
+  if (!terminalMarble) {
+    return input.map((_) => createEmptyMarble());
+  }
+  const terminalIndex = input.indexOf(terminalMarble);
+
+  const max = input
+    .map((marble) => marble.value)
+    .filter((value) => value != null)
+    .sort((a, b) => (a! > b! ? 1 : -1))[0];
+
+  return [...Array(input.length)].map((_, index) => {
+    if (index < terminalIndex) {
+      return createEmptyMarble();
+    } else {
+      return createTerminalMarble(max);
+    }
+  });
 }
