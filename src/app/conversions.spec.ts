650047337a48ee0e2a97fde849608497d3753c6e
Joe Chapman <joe.chapman3737@gmail.com>
2022-05-21 22:49:08 +1200

    implementation for diagram/marble conversions

--- src/app/conversions.spec.ts
+++ src/app/conversions.spec.ts
@@ -1,5 +1,10 @@
-import { diagramToMarbles } from './conversions';
-import { MarbleDiagram } from './types';
+import { diagramToMarbles, marblesToDiagram } from './conversions';
+import {
+  createEmptyMarble,
+  createTerminalMarble,
+  MarbleDiagram,
+  MarbleValue,
+} from './types';
 
 describe('diagramToMarbles', () => {
   it('should map "-" to empty marble', () => {
@@ -12,5 +17,133 @@ describe('diagramToMarbles', () => {
 
     expect(marbles.length).toBe(1);
     expect(marbles[0].value).toBeNull();
+    expect(marbles[0].terminal).toBeFalse();
+  });
+
+  it('should map "(|)" to terminal marble', () => {
+    const marbleDiagram: MarbleDiagram = {
+      diagram: '(|)',
+      values: {},
+    };
+
+    const marbles = diagramToMarbles(marbleDiagram);
+
+    expect(marbles.length).toBe(1);
+    expect(marbles[0].value).toBeNull();
+    expect(marbles[0].terminal).toBeTrue();
+  });
+
+  it('should map "(a)" to marble with value', () => {
+    const marbleDiagram: MarbleDiagram = {
+      diagram: '(a)',
+      values: { a: 1 },
+    };
+
+    const marbles = diagramToMarbles(marbleDiagram);
+
+    expect(marbles.length).toBe(1);
+    expect(marbles[0].value).toBe(1);
+    expect(marbles[0].terminal).toBeFalse();
+  });
+
+  it('should map "(a|)" to marble with value and completion', () => {
+    const marbleDiagram: MarbleDiagram = {
+      diagram: '(a|)',
+      values: { a: 1 },
+    };
+
+    const marbles = diagramToMarbles(marbleDiagram);
+
+    expect(marbles.length).toBe(1);
+    expect(marbles[0].value).toBe(1);
+    expect(marbles[0].terminal).toBeTrue();
+  });
+
+  it('should map "(ab)" to marble with multiple values', () => {
+    const marbleDiagram: MarbleDiagram = {
+      diagram: '(ab)',
+      values: { a: 1, b: 2 },
+    };
+
+    const marbles = diagramToMarbles(marbleDiagram);
+
+    expect(marbles.length).toBe(1);
+    expect(marbles[0].value).toBe(1);
+    expect(marbles[0].secondaryValue).toBe(2);
+    expect(marbles[0].terminal).toBeFalse();
+  });
+
+  it('should map "(ab|)" to marble with multiple values and completion', () => {
+    const marbleDiagram: MarbleDiagram = {
+      diagram: '(ab|)',
+      values: { a: 1, b: 2 },
+    };
+
+    const marbles = diagramToMarbles(marbleDiagram);
+
+    expect(marbles.length).toBe(1);
+    expect(marbles[0].value).toBe(1);
+    expect(marbles[0].secondaryValue).toBe(2);
+    expect(marbles[0].terminal).toBeTrue();
+  });
+});
+
+describe('marblesToDiagram', () => {
+  it('should map empty marble to "-"', () => {
+    const marbles = [createEmptyMarble()];
+
+    const diagram = marblesToDiagram(marbles);
+
+    expect(diagram.diagram).toBe('-');
+    expect(diagram.values).toEqual({});
+  });
+
+  it('should map completion marble with no value to "(|)"', () => {
+    const marbles = [createTerminalMarble()];
+
+    const diagram = marblesToDiagram(marbles);
+
+    expect(diagram.diagram).toBe('(|)');
+    expect(diagram.values).toEqual({});
+  });
+
+  it('should map marble with single value to "(a)"', () => {
+    const marbles: MarbleValue[] = [{ value: 1, terminal: false }];
+
+    const diagram = marblesToDiagram(marbles);
+
+    expect(diagram.diagram).toBe('(a)');
+    expect(diagram.values).toEqual({ a: 1 });
+  });
+
+  it('should map marble with single value with completion to "(a|)"', () => {
+    const marbles: MarbleValue[] = [{ value: 1, terminal: true }];
+
+    const diagram = marblesToDiagram(marbles);
+
+    expect(diagram.diagram).toBe('(a|)');
+    expect(diagram.values).toEqual({ a: 1 });
+  });
+
+  it('should map marble with multiple values to "(ab)"', () => {
+    const marbles: MarbleValue[] = [
+      { value: 1, secondaryValue: 2, terminal: false },
+    ];
+
+    const diagram = marblesToDiagram(marbles);
+
+    expect(diagram.diagram).toBe('(ab)');
+    expect(diagram.values).toEqual({ a: 1, b: 2 });
+  });
+
+  it('should map marble with multiple values with completion to "(ab|)"', () => {
+    const marbles: MarbleValue[] = [
+      { value: 1, secondaryValue: 2, terminal: true },
+    ];
+
+    const diagram = marblesToDiagram(marbles);
+
+    expect(diagram.diagram).toBe('(ab|)');
+    expect(diagram.values).toEqual({ a: 1, b: 2 });
   });
 });
--- src/app/conversions.ts
+++ src/app/conversions.ts
@@ -3,25 +3,34 @@ import { createEmptyMarble, MarbleDiagram, MarbleValue } from './types';
 export function diagramToMarbles(diagram: MarbleDiagram): MarbleValue[] {
   const marbles: MarbleValue[] = [];
   let index = 0;
-  index; /*?*/
   while (index < diagram.diagram.length) {
     if (diagram.diagram[index] === '-') {
       marbles.push(createEmptyMarble());
       index++;
     } else if (diagram.diagram[index] === '(') {
       index++;
-      // const marbleForCurrentTick = createEmptyMarble();
+      const marbleForCurrentTick = createEmptyMarble();
+      marbles.push(marbleForCurrentTick);
       while (diagram.diagram[index] != ')') {
-        //   if (diagram.diagram[index] == '|') {
-        //     marbleForCurrentTick.terminal = true;
-        //   } else if (marbleForCurrentTick.value == null) {
-        //     marbleForCurrentTick.value = diagram.values[diagram.diagram[index]];
-        //   } else {
-        //     marbleForCurrentTick.secondaryValue =
-        //       diagram.values[diagram.diagram[index]];
-        //   }
+        if (diagram.diagram[index] == '|') {
+          marbleForCurrentTick.terminal = true;
+        } else if (marbleForCurrentTick.value == null) {
+          marbleForCurrentTick.value = diagram.values[diagram.diagram[index]];
+        } else {
+          marbleForCurrentTick.secondaryValue =
+            diagram.values[diagram.diagram[index]];
+        }
         index++;
       }
+      index++;
+    } else {
+      console.error(
+        'unexpected value in diagram',
+        diagram.diagram,
+        index,
+        diagram.diagram[index]
+      );
+      index++;
     }
   }
 
@@ -29,11 +38,66 @@ export function diagramToMarbles(diagram: MarbleDiagram): MarbleValue[] {
 }
 
 export function marblesToDiagram(marbles: MarbleValue[]): MarbleDiagram {
-  return { diagram: marbles.map((x) => '-').join(''), values: {} };
-}
+  const keys = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  let keyIndex = 0;
+  const marbleDiagram: MarbleDiagram = {
+    diagram: '',
+    values: {},
+  };
 
-const diagram: MarbleDiagram = {
-  diagram: '(|)',
-  values: { a: 1, b: 2 },
-};
-diagramToMarbles(diagram); /*?*/
+  marbles.forEach((marble) => {
+    if (!marble.value) {
+      if (!marble.terminal) {
+        marbleDiagram.diagram = marbleDiagram.diagram + '-';
+      } else {
+        marbleDiagram.diagram = marbleDiagram.diagram + '(|)';
+      }
+    } else {
+      marbleDiagram.diagram = marbleDiagram.diagram + '(';
+      marbleDiagram.diagram = marbleDiagram.diagram + keys[keyIndex];
+      marbleDiagram.values[keys[keyIndex]] = marble.value;
+      keyIndex++;
+
+      if (marble.secondaryValue != null) {
+        marbleDiagram.diagram = marbleDiagram.diagram + keys[keyIndex];
+        marbleDiagram.values[keys[keyIndex]] = marble.secondaryValue;
+        keyIndex++;
+      }
+
+      if (marble.terminal) {
+        marbleDiagram.diagram = marbleDiagram.diagram + '|';
+      }
+
+      marbleDiagram.diagram = marbleDiagram.diagram + ')';
+    }
+  });
+
+  return marbleDiagram;
+}
