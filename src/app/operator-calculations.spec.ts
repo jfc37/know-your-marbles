1745cfee7c3d01de957889015140a54777e89d93
Joe Chapman <joe.chapman3737@gmail.com>
2022-05-22 20:30:40 +1200

    first operation

--- src/app/operator-calculations.spec.ts
+++ src/app/operator-calculations.spec.ts
@@ -1,123 +1,174 @@
 import { cold } from 'jasmine-marbles';
-import { max, min } from 'rxjs';
-import { maxCalculation, minCalculation } from './operator-calculations';
+import { first, max, min, OperatorFunction, takeUntil } from 'rxjs';
+import { getCalculationFn } from './operator-calculations';
+import { MarbleDiagram, Operations } from './types';
 
-describe('maxCalculation', () => {
-  it('should be "-----" when input is "-----"', () => {
-    const diagram = '-----';
-    const values = { a: 2, b: 1 };
-
-    runTest(diagram, values);
-  });
-
-  it('should be "----|" when input is "----|"', () => {
-    const diagram = '----|';
-    const values = {};
-
-    runTest(diagram, values);
-  });
-
-  it('should be "----(2|)" when input is "2-1-|"', () => {
-    const diagram = 'a-b-|';
-    const values = { a: 2, b: 1 };
-
-    runTest(diagram, values);
-  });
-
-  it('should be "----(3|)" when input is "(31)-2-|"', () => {
-    const diagram = '(ab)-c-|';
-    const values = { a: 3, b: 1, c: 2 };
-    const expectedOutput = '----(a|)';
-    const expectedValues = { a: 3 };
-
-    runTestWithGrouping(diagram, values, expectedOutput, expectedValues);
-  });
-
-  function runTest(diagram: string, values: { [key: string]: number }): void {
-    const realInput = cold(diagram, values);
-    const realOutput = realInput.pipe(max());
-
-    const simulatedOutput = maxCalculation({
-      diagram: diagram,
-      values: values,
-    });
-    expect(realOutput).toBeObservable(
-      cold(simulatedOutput.diagram, simulatedOutput.values)
+describe('first', () => {
+  it('check "-----" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: '-----',
+        values: {},
+      },
+      Operations.First,
+      first()
     );
-  }
+  });
 
-  function runTestWithGrouping(
-    diagram: string,
-    values: { [key: string]: number },
-    expectedOutput: string,
-    expectedValues: { [key: string]: number }
-  ): void {
-    const simulatedOutput = maxCalculation({
-      diagram: diagram,
-      values: values,
-    });
-    expect(
-      cold(simulatedOutput.diagram, simulatedOutput.values)
-    ).toBeObservable(cold(expectedOutput, expectedValues));
-  }
+  // TODO: Need to implement errors
+  xit('check "----|" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: '----|',
+        values: {},
+      },
+      Operations.First,
+      first()
+    );
+  });
+
+  it('check "2-1--" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: '-a-b--',
+        values: { a: 2, b: 1 },
+      },
+      Operations.First,
+      first()
+    );
+  });
+
+  it('should be "--(1|)" when input is "--(13)-2-"', () => {
+    const input = {
+      diagram: '--(ab)-c-',
+      values: { a: 1, b: 3, c: 2 },
+    };
+    const expectedOutput = {
+      diagram: '--(a|)',
+      values: { a: 1 },
+    };
+
+    compareWithExpected(input, Operations.First, expectedOutput);
+  });
 });
 
-describe('minCalculation', () => {
-  it('should be "-----" when input is "-----"', () => {
-    const diagram = '-----';
-    const values = { a: 2, b: 1 };
-
-    runTest(diagram, values);
+describe('max', () => {
+  it('check "-----" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: '-----',
+        values: {},
+      },
+      Operations.Max,
+      max()
+    );
   });
 
-  it('should be "----|" when input is "----|"', () => {
-    const diagram = '----|';
-    const values = {};
-
-    runTest(diagram, values);
+  it('check "----|" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: '----|',
+        values: {},
+      },
+      Operations.Max,
+      max()
+    );
   });
 
-  it('should be "----(1|)" when input is "2-1-|"', () => {
-    const diagram = 'a-b-|';
-    const values = { a: 2, b: 1 };
+  it('check "2-1--" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: 'a-b-|',
+        values: { a: 2, b: 1 },
+      },
+      Operations.Max,
+      max()
+    );
+  });
 
-    runTest(diagram, values);
+  it('should be "----(3|)" when input is "(13)-2-|"', () => {
+    const input = {
+      diagram: '(ab)-c-|',
+      values: { a: 1, b: 3, c: 2 },
+    };
+    const expectedOutput = {
+      diagram: '----(a|)',
+      values: { a: 3 },
+    };
+
+    compareWithExpected(input, Operations.Max, expectedOutput);
+  });
+});
+
+describe('min', () => {
+  it('check "-----" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: '-----',
+        values: {},
+      },
+      Operations.Min,
+      min()
+    );
+  });
+
+  it('check "----|" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: '----|',
+        values: {},
+      },
+      Operations.Min,
+      min()
+    );
+  });
+
+  it('check "2-1--" has same output as rxjs', () => {
+    compareWithRxjs(
+      {
+        diagram: 'a-b-|',
+        values: { a: 2, b: 1 },
+      },
+      Operations.Min,
+      min()
+    );
   });
 
   it('should be "----(1|)" when input is "(21)-3-|"', () => {
-    const diagram = '(ab)-c-|';
-    const values = { a: 2, b: 1, c: 3 };
-    const expectedOutput = '----(a|)';
-    const expectedValues = { a: 1 };
+    const input = {
+      diagram: '(ab)-c-|',
+      values: { a: 2, b: 1, c: 3 },
+    };
+    const expectedOutput = {
+      diagram: '----(a|)',
+      values: { a: 1 },
+    };
 
-    runTestWithGrouping(diagram, values, expectedOutput, expectedValues);
+    compareWithExpected(input, Operations.Min, expectedOutput);
   });
-
-  function runTest(diagram: string, values: { [key: string]: number }): void {
-    const realInput = cold(diagram, values);
-    const realOutput = realInput.pipe(min());
-
-    const simulatedOutput = minCalculation({
-      diagram: diagram,
-      values: values,
-    });
-    expect(realOutput).toBeObservable(
-      cold(simulatedOutput.diagram, simulatedOutput.values)
-    );
-  }
-
-  function runTestWithGrouping(
-    diagram: string,
-    values: { [key: string]: number },
-    expectedOutput: string,
-    expectedValues: { [key: string]: number }
-  ): void {
-    const simulatedOutput = minCalculation({
-      diagram: diagram,
-      values: values,
-    });
-    expect(
-      cold(simulatedOutput.diagram, simulatedOutput.values)
-    ).toBeObservable(cold(expectedOutput, expectedValues));
-  }
 });
+
+function compareWithRxjs<TInput, TOutput>(
+  marbleDiagram: MarbleDiagram,
+  operation: Operations,
+  rxjsOperation: OperatorFunction<TInput, TOutput>
+): void {
+  const realInput = cold(marbleDiagram.diagram, marbleDiagram.values);
+  const realOutput = realInput.pipe(rxjsOperation);
+
+  const simulatedOutput = getCalculationFn(operation)(marbleDiagram);
+  expect(realOutput).toBeObservable(
+    cold(simulatedOutput.diagram, simulatedOutput.values)
+  );
+}
+
+function compareWithExpected(
+  marbleDiagram: MarbleDiagram,
+  operation: Operations,
+  expectedOutput: MarbleDiagram
+): void {
+  const simulatedOutput = getCalculationFn(operation)(marbleDiagram);
+  expect(cold(simulatedOutput.diagram, simulatedOutput.values)).toBeObservable(
+    cold(expectedOutput.diagram, expectedOutput.values)
+  );
+}
--- src/app/operator-calculations.ts
+++ src/app/operator-calculations.ts
@@ -1,11 +1,5 @@
 import { diagramToMarbles } from './conversions';
-import {
-  createEmptyMarble,
-  createTerminalMarble,
-  MarbleDiagram,
-  MarbleValue,
-  Operations,
-} from './types';
+import { MarbleDiagram, MarbleValue, Operations } from './types';
 
 const OPERATION_CALC_MAP = {
   [Operations.First]: firstCalculation,
@@ -36,35 +30,38 @@ export function takeUntilCalculation(
 }
 
 export function firstCalculation(input: MarbleDiagram): MarbleDiagram {
-  return input;
+  const marbles = diagramToMarbles(input);
+
+  if (neverEmits(input)) {
+    return getEmptyMarbleDiagram(marbles.length);
+  }
+
+  const firstEmissionTick = getFirstEmissionTick(marbles);
+  return {
+    diagram: getEmptyDiagram(firstEmissionTick) + '(a|)',
+    values: { a: input.values['a'] },
+  };
 }
 
 export function maxCalculation(input: MarbleDiagram): MarbleDiagram {
   const marbles = diagramToMarbles(input);
-  const completion = marbles.find((marble) => marble.terminal);
 
-  if (!completion) {
-    return { diagram: marbles.map((_) => '-').join(''), values: {} };
+  if (neverCompletes(marbles)) {
+    return getEmptyMarbleDiagram(marbles.length);
   }
+  const completionTick = getCompletionTick(marbles);
 
-  const max = [...Object.values(input.values)].sort((a, b) =>
-    a > b ? -1 : 1
-  )[0];
-  const completionTick = marbles.indexOf(completion);
-
-  if (max == null) {
+  if (neverEmits(input)) {
     return {
-      diagram: [[...Array(completionTick)].map((_) => '-').join(''), '|'].join(
-        ''
-      ),
+      diagram: getEmptyDiagram(completionTick) + '|',
       values: {},
     };
   }
 
+  const max = getEmittedValues(input).sort(orderAscending)[0];
+
   return {
-    diagram: [[...Array(completionTick)].map((_) => '-').join(''), '(a|)'].join(
-      ''
-    ),
+    diagram: getEmptyDiagram(completionTick) + '(a|)',
     values: {
       a: max,
     },
@@ -73,32 +70,63 @@ export function maxCalculation(input: MarbleDiagram): MarbleDiagram {
 
 export function minCalculation(input: MarbleDiagram): MarbleDiagram {
   const marbles = diagramToMarbles(input);
-  const completion = marbles.find((marble) => marble.terminal);
 
-  if (!completion) {
-    return { diagram: marbles.map((_) => '-').join(''), values: {} };
+  if (neverCompletes(marbles)) {
+    return getEmptyMarbleDiagram(marbles.length);
   }
+  const completionTick = getCompletionTick(marbles);
 
-  const min = [...Object.values(input.values)].sort((a, b) =>
-    a < b ? -1 : 1
-  )[0];
-  const completionTick = marbles.indexOf(completion);
-
-  if (min == null) {
+  if (neverEmits(input)) {
     return {
-      diagram: [[...Array(completionTick)].map((_) => '-').join(''), '|'].join(
-        ''
-      ),
+      diagram: getEmptyDiagram(completionTick) + '|',
       values: {},
     };
   }
 
+  const min = getEmittedValues(input).sort(orderDescending)[0];
+
   return {
-    diagram: [[...Array(completionTick)].map((_) => '-').join(''), '(a|)'].join(
-      ''
-    ),
+    diagram: getEmptyDiagram(completionTick) + '(a|)',
     values: {
       a: min,
     },
   };
+}
+
+function neverCompletes(marbles: MarbleValue[]): boolean {
+  return !marbles.some((marble) => marble.terminal);
+}
+
+function getEmptyMarbleDiagram(ticks: number): MarbleDiagram {
+  return { diagram: getEmptyDiagram(ticks), values: {} };
+}
+
+function getEmptyDiagram(ticks: number): string {
+  return [...Array(ticks)].map((_) => '-').join('');
+}
+
+function getFirstEmissionTick(marbles: MarbleValue[]): number {
+  const first = marbles.find((marble) => marble.value);
+  return marbles.indexOf(first!);
+}
+
+function getCompletionTick(marbles: MarbleValue[]): number {
+  const completion = marbles.find((marble) => marble.terminal);
+  return marbles.indexOf(completion!);
+}
+
+function getEmittedValues(marbleDiagram: MarbleDiagram): number[] {
+  return [...Object.values(marbleDiagram.values)];
+}
+
+function neverEmits(marbleDiagram: MarbleDiagram): boolean {
+  return Object.values(marbleDiagram.values).length === 0;
+}
+
+function orderAscending(a: number, b: number) {
+  return a > b ? -1 : 1;
+}
+
+function orderDescending(a: number, b: number) {
+  return a < b ? -1 : 1;
 }
