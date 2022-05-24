8116914c773b31d1af4d1e891d4d026f14e6bc36
Joe Chapman <joe.chapman3737@gmail.com>
2022-05-24 19:55:08 +1200

    shifted operation specs

---
+++ src/app/logic/operations/max.spec.ts
@@ -0,0 +1,29 @@
+import { max } from 'rxjs/operators';
+import { Diagram } from '../diagram';
+import { Operations } from '../operation-map';
+import { compareWithExpected, compareWithRxjs } from './operation.utils.spec';
+
+describe('max', () => {
+  it('check "-----" has same output as rxjs', () => {
+    compareWithRxjs(Diagram.create('-----'), Operations.Max, max());
+  });
+
+  it('check "----|" has same output as rxjs', () => {
+    compareWithRxjs(Diagram.create('----|'), Operations.Max, max());
+  });
+
+  it('check "2-1--" has same output as rxjs', () => {
+    compareWithRxjs(
+      Diagram.create('a-b-|', { a: 2, b: 1 }),
+      Operations.Max,
+      max()
+    );
+  });
+
+  it('should be "----(3|)" when input is "(13)-2-|"', () => {
+    const input = Diagram.create('(ab)-c-|', { a: 1, b: 3, c: 2 });
+    const expectedOutput = Diagram.create('----(a|)', { a: 3 });
+
+    compareWithExpected(input, Operations.Max, expectedOutput);
+  });
+});
---
+++ src/app/logic/operations/takeUntil.spec.ts
@@ -0,0 +1,33 @@
+import { takeUntil } from 'rxjs/operators';
+import { Diagram } from '../diagram';
+import { Operations } from '../operation-map';
+import { binaryCompareWithRxjs } from './operation.utils.spec';
+
+describe('take until', () => {
+  it('check "abcde" | "----" has same output as rxjs', () => {
+    binaryCompareWithRxjs(
+      Diagram.create('abcde', { a: 1, b: 2, c: 3, d: 4, e: 5 }),
+      Operations.TakeUntil,
+      Diagram.create('----'),
+      takeUntil
+    );
+  });
+
+  it('check "abcde" | "--|" has same output as rxjs', () => {
+    binaryCompareWithRxjs(
+      Diagram.create('abcde', { a: 1, b: 2, c: 3, d: 4, e: 5 }),
+      Operations.TakeUntil,
+      Diagram.create('--|'),
+      takeUntil
+    );
+  });
+
+  it('check "abcde" | "--f-g" has same output as rxjs', () => {
+    binaryCompareWithRxjs(
+      Diagram.create('abcde', { a: 1, b: 2, c: 3, d: 4, e: 5 }),
+      Operations.TakeUntil,
+      Diagram.create('--f-g', { f: 1, g: 1 }),
+      takeUntil
+    );
+  });
+});
